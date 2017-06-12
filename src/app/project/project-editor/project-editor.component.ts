25b2efe1de554d80054f7a62700b4ec705153e55
constantin.mueller <constantin.mueller@kws.com>
2017-06-12 12:41:23 +0200

    added project overview component and extracted rounting information

--- src/app/project/project-editor/project-editor.component.ts
+++ src/app/project/project-editor/project-editor.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {ProjectService} from '../project.service';
-import {Project} from '../project';
+import 'rxjs/add/operator/switchMap';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import { Location } from '@angular/common';
+import { ProjectService } from '../project.service';
+import { Project } from '../project';
+import {log} from 'util';
 
 @Component({
   selector: 'app-project-editor',
@@ -11,12 +15,47 @@ export class ProjectEditorComponent implements OnInit {
 
   public currentProject: Project;
 
-  constructor(private projectService: ProjectService) {
-
+  constructor(
+    private projectService: ProjectService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location) {
+    this.currentProject = new Project();
+    log('done constructing');
   }
+
+// The switchMap operator maps the id in the Observable route parameters to a new Observable,
+// the result of the HeroService.getHero() method.
+// The id is a number. Route parameters are always strings. So the route parameter value is
+// converted to a number with the JavaScript (+) operator.
 
   ngOnInit() {
-    this.currentProject = this.projectService.createProject();
+    // (+) converts string 'id' to a number
+    this.route.params
+      .switchMap((params: Params) => this.projectService.getProject(+params['id']))
+      .subscribe(project => this.currentProject = project);
   }
 
+  public saveProject() {
+    log('saving project');
+    this.projectService.saveProject(this.currentProject).then(
+      p => {
+        const isNew = !(this.currentProject.id > 0);
+        this.currentProject = p;
+        if (! isNew) {
+          this.router.navigate(['/editor'.concat('' + p.id)]);
+        }
+      }
+    );
+  }
+
+  public newProject() {
+    log('creating new project');
+    this.projectService.createProject().then(
+      p => {
+        log('created new project: ' + p.id);
+        this.router.navigate(['/editor/' + p.id]);
+      }
+    );
+  }
 }
---
+++ src/app/project/project-overview/project-overview.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ProjectService} from '../project.service';
+
+@Component({
+  selector: 'app-project-overview',
+  templateUrl: './project-overview.component.html',
+  styleUrls: ['./project-overview.component.less']
+})
+export class ProjectOverviewComponent implements OnInit {
+
+  public projectList: Array<{id: number, title: string}>;
+
+  constructor(private projectService: ProjectService) {
+    this.projectList = new Array();
+  }
+
+  ngOnInit() {
+      this.projectService.getProjectOverview().then(projects => this.projectList = projects);
+  }
+
+}
